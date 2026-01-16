78edccc0223daf05764a5b15b61e0e1de33a6605
ASUS <faris13abiyu@gmail.com>
2026-01-16 15:23:28 +0700

    membuat dan mengisi file database baru : FoodDatabase

---
+++ app/src/main/java/com/example/nutriscan/room/FoodDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.nutriscan.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [FoodEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class FoodDatabase : RoomDatabase() {
+
+    abstract fun foodDao(): FoodDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FoodDatabase? = null
+
+        fun getDatabase(context: Context): FoodDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FoodDatabase::class.java,
+                    "nutriscan_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
