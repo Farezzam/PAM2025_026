2998591db9fa55d309ab84f470a73c48856d208d
ASUS <faris13abiyu@gmail.com>
2026-01-17 15:30:12 +0700

    membuat gradientbutton

--- app/src/main/java/com/example/nutriscan/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nutriscan/ui/theme/Theme.kt
@@ -2,14 +2,29 @@ package com.example.nutriscan.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -31,6 +46,10 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
+)
+
+val GradasiHijau = Brush.horizontalGradient(
+    colors = listOf(Color(0xFF2A762E), Color(0xFF08AF11))
 )
 
 @Composable
@@ -55,4 +74,29 @@ fun NutriScanTheme(
         typography = Typography,
         content = content
     )
+}
+
+@Composable
+fun GradientButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button (
+        onClick = onClick,
+        modifier = modifier,
+        contentPadding = PaddingValues(), // Agar gradasi menutupi seluruh tombol
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+        shape = RoundedCornerShape(12.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(GradasiHijau)
+                .fillMaxWidth()
+                .padding(vertical = 12.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = text, color = Color.White, fontWeight = FontWeight.Bold)
+        }
+    }
 }
